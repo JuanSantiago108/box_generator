4f2f1099a861a27afda4a9070d3f92cc2a5af38d
JuanSantiago108 <santiagojuanandres@gmail.com>
2022-07-10 19:54:23 -0400

    first commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,53 @@
+import { buildQueries } from '@testing-library/react';
+import React, { useState } from 'react'
+
+const BoxGenerator = (props) => {
+    const [boxColor, setBoxColor] = useState("");
+    const [boxSize, setBoxSize] = useState("");
+
+    const[boxList, setBoxList] = useState([])
+
+    const createBox = (e) =>{
+        e.preventDefault();
+        const newBox ={boxColor, boxSize};
+        setBoxList ([newBox, ...boxList]);
+        console.log(boxList)
+    }
+
+
+
+
+return (
+    <div>
+        <form onSubmit={createBox} >
+
+            <div>
+                <label>Box Color:</label>
+                <input type="text" name="boxColor" 
+                onChange = {(e) => setBoxColor(e.target.value)}
+                value = {boxColor}></input>
+            </div>
+
+            <div>
+                <label>Box Size:</label>
+                <input type="number"  name="boxSize" 
+                onChange={(e) => setBoxSize(parseInt(e.target.value))}
+                value = {boxSize}></input>
+            </div>
+
+            <button type='submit'> Create Box</button>
+
+        </form>
+
+        <div className='sort'>
+        {
+            boxList.map((b => <div style={{backgroundColor: b.boxColor, height: b.boxSize, width: b.boxSize}}></div>))
+        }
+        </div>
+
+
+    </div>
+)
+}
+
+export default BoxGenerator
